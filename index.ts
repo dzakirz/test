7264f0280dd84eb95ab44642d7e6caaa6e3aa4c1
Dzaki Rozaan <dzakirz.dev@gmail.com>
2023-09-13 19:56:02 +0700

    fix functionallity

--- index.ts
+++ index.ts
@@ -1,9 +1,1 @@
-const test = {
-  sayHello: (name: string): string => {
-    const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1)
-
-    return `Hello ${capitalizedName}!`
-  }
-}
-
-export default test
+export const test = () => "Test"
